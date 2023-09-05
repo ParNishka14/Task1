92c7857de32f8eb697611062fdbeb189a057c256
unknown <admin@3205EC06.att.edu.ru>
2023-09-05 11:58:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Methods.printThreeWords();
+        Methods.compareNumbers(1,4);
+        Methods.сheckSumSign(1,4);
+        Methods.printColor(22);
+        }
+    }
